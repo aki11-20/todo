d9d3eef9944fd0a928ac6ffb4467c56e16700944
tsushima aki <aki1120tsushima@gmail.com>
2025-08-21 17:29:43 +0900

    終了

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,10 +9,20 @@
     <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
     @yield('css')
 </head>
+
 <body>
     <header class="header">
         <div class="header__inner">
-            <a class="header__logo" href="/">Todo</a>
+            <div class="header-utilities">
+                <a class="header__logo" href="/">Todo</a>
+                <nav>
+                    <ul class="header-nav">
+                        <li class="header-nav__item">
+                            <a class="header-nav__link" href="/categories">カテゴリ一覧</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     </header>
 
@@ -19,4 +30,5 @@
         @yield('content')
     </main>
 </body>
+
 </html>
